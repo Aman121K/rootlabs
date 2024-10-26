c04f1f930754441e7c01720a84acec64a2873943
Vikas Tiwari <94979466+Aman121K@users.noreply.github.com>
2024-10-26 16:07:29 +0530

    rename

--- src/components/cookiePoopup/CookiePopup.jsx
+++ src/components/cookiePoopup/CookiePopup.jsx
@@ -18,40 +18,40 @@ const CookiePopup = () => {
 
   return (
     <>
-        {isVisible && (
-      <div className="cookie-popup-overlay">
-        <div className="cookie-popup">
-          <h2>Cookie Settings</h2>
-          <p>
-            By clicking “Accept All”, you consent to all defined categories of
-            cookies including analytics and targeting cookies.{" "}
-            <a href="/privacy-policy">Privacy Policy</a>
-          </p>
-          <div className="button-group">
-            <button className="reject-button" onClick={handleRejectAll}>
-              Reject All
-            </button>
-            <button className="accept-button" onClick={handleAcceptAll}>
-              Accept All
-            </button>
+      {isVisible && (
+        <div className="cookie-popup-overlay">
+          <div className="cookie-popup">
+            <h2>Cookie Settings</h2>
+            <p>
+              By clicking “Accept All”, you consent to all defined categories of
+              cookies including analytics and targeting cookies.{" "}
+              <a href="/privacy-policy">Privacy Policy</a>
+            </p>
+            <div className="button-group">
+              <button className="reject-button" onClick={handleRejectAll}>
+                Reject All
+              </button>
+              <button className="accept-button" onClick={handleAcceptAll}>
+                Accept All
+              </button>
+            </div>
           </div>
-        </div>
-      </div>)}
+        </div>)}
       {showBanner && (
-  <div className="cookie-banner">
-    <p>
-      We use third-party cookies across all defined categories of cookies
-      including analytics and targeting cookies.{" "}
-      <a href="/terms-and-conditions">Privacy Policy</a>
-    </p>
-    <button className="close-banner" onClick={() => setShowBanner(false)}>
-      &times;
-    </button>
-  </div>
-)}
+        <div className="cookie-banner">
+          <p>
+            We use third-party cookies across all defined categories of cookies
+            including analytics and targeting cookies.{" "}
+            <a href="/terms-and-conditions">Privacy Policy</a>
+          </p>
+          <button className="close-banner" onClick={() => setShowBanner(false)}>
+            &times;
+          </button>
+        </div>
+      )}
 
     </>
-    )
+  )
 };
 
 export default CookiePopup;
